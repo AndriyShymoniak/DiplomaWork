4b6867aee0a78caf8490ffbc84849ea4a0e930ef
Andriy <andrysimonyak@gmail.com>
2021-03-04 15:00:48 +0200

    Routing added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { RecognizepageComponent } from './recognizepage/recognizepage.component';
+import { MappageComponent } from './mappage/mappage.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainpageComponent
+    MainpageComponent,
+    RecognizepageComponent,
+    MappageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
