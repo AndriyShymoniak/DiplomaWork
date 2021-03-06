2b2b580e772d154addaf94f67b7e58159a0edbbc
Andriy <andrysimonyak@gmail.com>
2021-03-06 18:05:47 +0200

    Cloudinary updated

--- backend/recognize_app/rec_api/models.py
+++ backend/recognize_app/rec_api/models.py
@@ -6,4 +6,7 @@ class RecognizedObject(models.Model):
     latitude = models.CharField(max_length=20)
     longitude = models.CharField(max_length=20)
     description = models.CharField(max_length=200, default='empty description')
-    image = CloudinaryField('image', default='https://res.cloudinary.com/ddmivk4av/image/upload/v1615040123/diplomawork/not_found.jpg')
+    image = CloudinaryField('image',
+                            format='jpg',
+                            folder='diplomawork',
+                            default='https://res.cloudinary.com/ddmivk4av/image/upload/v1615040123/diplomawork/not_found.jpg')
